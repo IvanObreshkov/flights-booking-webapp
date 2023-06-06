fb24e78a275e1e2a22f98ab34a560dbc04226ea5
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-06-06 16:59:10 +0300

    Create get users route
    Get data from POST request and add it to DB

--- api/routes/register_route.py
+++ api/routes/register_route.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from flask import Blueprint
 from flask import request
 from flask_expects_json import expects_json
@@ -19,37 +20,62 @@ schema = {
     'required': ['first_name', 'last_name', 'email', 'password']
 }
 
-@register_bp.route("/register", methods=('GET', 'POST'))
-@expects_json(schema, ignore_for=['GET'])
+
+@register_bp.route("/register", methods=['POST'])
+@expects_json(schema)
 def register():
-    if request.method == "GET":
-        response = {"Message": "Welcome!"}
-        try:
-            # TODO: Load register form
-            result = db.session.execute(text("Select * From users"))
-            rows = result.all()
-            for row in rows:
-                print(row)
-            db.session.close()
-            return response
-        except Exception as e:
-            return {"Message": "Couldn't load register form", "Error": str(e)}, 500
-    else:
-        json_data = request.json
-        # TODO:
-        #  - Check if user exists in DB: if true -> return Bad Request, else -> add user to DB
-        #  (later add verification email )
-        #  - Hash password
-        user = Users(
-            first_name="Toni",
-            last_name="Montana",
-            email="misho1234@abv.bg",
-            password="pass1234"
+    json_data = request.json
+    first_name = json_data["first_name"]
+    last_name = json_data["last_name"]
+    email = json_data["email"]
+    password = json_data["password"]
+
+    # TODO:
+    #  - Hash password
+    #  - Implement JWT
+    #  - Send verification emails
+
+    try:
+        new_user = Users(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
         )
-        db.session.add(user)
+        db.session.add(new_user)
         db.session.commit()
         db.session.close()
-        return {"Message": "Ok"}
+        return {"Message": "New user added to DB!"}
 
-def check_if_user_exists():
-    pass
+    except sqlalchemy.exc.IntegrityError as e:
+        return {"Error": f"Email address {email} exist in the DB!"}, 409
+
+
+@register_bp.route("/users", methods=['GET'])
+def get_users():
+    try:
+        result = db.session.execute(text("Select * From users"))
+        rows = result.all()
+
+        # JSON serializing of Row objects
+        users = serialize_rows(result, rows)
+        response = {"Users": users}
+
+        db.session.close()
+        return response
+
+    except Exception as e:
+        return {"Message": "Couldn't load register form", "Error": str(e)}, 500
+
+
+def serialize_rows(result, rows):
+    """Serializes Row objects to JSON so that they could be returned as a response from the API"""
+
+    column_names = result.keys()
+    users = []
+    for row in rows:
+        user = {}
+        for column, value in zip(column_names, row):
+            user[column] = value
+        users.append(user)
+    return users
