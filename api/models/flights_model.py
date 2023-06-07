cbab99575bb33d02aa35a34bd9e4de0304c2dfbe
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-06-07 19:48:59 +0300

    Fix generation of Flight number

--- api/models/flights_model.py
+++ api/models/flights_model.py
@@ -6,7 +6,7 @@ from database import db
 
 
 class Flights(db.Model):
-    flight_number = sqlalchemy.Column(sqlalchemy.String(6), primary_key=True, default=str(uuid.uuid4()),
+    flight_number = sqlalchemy.Column(sqlalchemy.String(6), primary_key=True, default=str(uuid.uuid4().hex)[:6].upper(),
                                       nullable=False, unique=True)
     start_destination = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
     end_destination = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
