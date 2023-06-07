37f14f964f92dc3d74232c343db7a6da4ab4a65d
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-06-08 02:06:35 +0300

    Create a update user endpoint

--- api/routes/rud_users_route.py
+++ api/routes/rud_users_route.py
@@ -3,11 +3,24 @@
 # https://flask.palletsprojects.com/en/2.3.x/tutorial/views/
 
 from flask import Blueprint, request
+from flask_expects_json import expects_json
 
+import models.users_model
 from database import db
 from models.users_model import Users
 
 rud_users_bp = Blueprint("crud_users", __name__)
+
+schema = {
+    'type': 'object',
+    'properties': {
+        'first_name': {'type': 'string'},
+        'last_name': {'type': 'string'},
+        'email': {'type': 'string', 'format': 'email'},
+        'password': {'type': 'string'}
+    },
+    'additionalProperties': False
+}
 
 
 @rud_users_bp.get("/users")
@@ -36,26 +49,56 @@ def get_user(user_uuid):
             return user.to_json(), 200
 
         return {"Message": f"User with uuid {user_uuid} doesn't exist in the DB!"}, 404
+
     except Exception as e:
         return {"Message": "Couldn't retrieve user from DB!", "Error": str(e)}, 500
+
     finally:
         db.session.close()
 
 
 @rud_users_bp.delete("/users/<uuid:user_uuid>")
 def delete_user(user_uuid):
+    # TODO: Add BEARER TOKEN
+
     try:
         user = db.session.query(Users).get(user_uuid)
         if user:
             db.session.delete(user)
-            raise Exception("Mama vi")
+            db.session.commit()
             return {"Message": f"User with uuid {user_uuid} was removed successfully from the DB"}, 200
+
         return {"Message": f"User with uuid {user_uuid} doesn't exist in the DB!"}, 404
+
     except Exception as e:
         db.session.rollback()
         return {"Message": "Couldn't retrieve user from DB!", "Error": str(e)}, 500
+
     finally:
-        db.session.commit()
         db.session.close()
 
-# TODO: Add PUT method
+
+@rud_users_bp.put("/users/<uuid:user_uuid>")
+@expects_json(schema,check_formats=True)
+def update_user(user_uuid):
+    # TODO: Add BEARER TOKEN
+
+    try:
+        user = db.session.query(Users).get(user_uuid)
+        if user:
+            json_data = request.json
+            user.first_name = json_data.get('first_name', user.first_name)
+            user.last_name = json_data.get('last_name', user.last_name)
+            user.email = json_data.get('email', user.email)
+            user.password = json_data.get('password', user.password)
+            db.session.commit()
+            return {"Message": f"User with uuid {user_uuid} was updated successfully."}, 200
+
+        return {"Message": f"User with uuid {user_uuid} doesn't exist in the DB!"}, 404
+
+    except Exception as e:
+        db.session.rollback()
+        return {"Message": "Couldn't retrieve user from DB!", "Error": str(e)}, 500
+
+    finally:
+        db.session.close()
