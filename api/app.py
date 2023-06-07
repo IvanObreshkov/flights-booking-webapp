377e3a8eb0c5fe2c80047f52d9fea9acfc16f872
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-06-07 21:40:21 +0300

    Register crud_users blueprint

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 
 from config import DevConfig
 from database import db
+from routes.crud_users_route import crud_users_bp
 
 from routes.register_route import register_bp
 
@@ -13,6 +14,7 @@ def create_app(config_class=DevConfig):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_class)
     app.register_blueprint(register_bp)
+    app.register_blueprint(crud_users_bp)
     db.init_app(app)
     with app.app_context():
         db.create_all()
