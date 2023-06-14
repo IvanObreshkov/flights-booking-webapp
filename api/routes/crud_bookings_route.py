0214b72a7aaf5ff5468bd0b231e1be35ead8afc6
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-06-14 18:17:59 +0300

    Create ednpoint for getting all flights by user_id
    Create endpooint for deleting booking by booking_id

--- api/routes/crud_bookings_route.py
+++ api/routes/crud_bookings_route.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 from database import db
 from models.user_bookings_model import UserBookings
+from models.users_model import Users
 
 crud_bookings_bp = Blueprint("bookings", __name__)
 
@@ -21,6 +22,8 @@ def get_bookings():
 
 @crud_bookings_bp.get("/bookings/<uuid:booking_id>")
 def get_booking(booking_id):
+    # TODO: Add Bearer Token
+
     try:
         booking = db.session.query(UserBookings).get(booking_id)
         if booking:
@@ -29,8 +32,48 @@ def get_booking(booking_id):
         return {"Message": f"Booking with uuid {booking_id} doesn't exist in the DB!"}, 404
 
     except Exception as e:
-        return {"Message": "Couldn't retrieve booking from DB!", "Error": str(e)}, 500
+        return {"Message": f"Couldn't retrieve booking with uuid {booking_id} from DB!", "Error": str(e)}, 500
 
     finally:
         db.session.close()
 
+@crud_bookings_bp.get("/bookings/<uuid:user_id>")
+def get_user_bookings(user_id):
+    # TODO: Add Bearer Token
+
+    try:
+        user = db.session.query(Users).get(user_id)
+        if user:
+            user_bookings = db.session.query(UserBookings).filter_by(UserBookings.user_id==user_id).all()
+
+            # TODO: Show all info for flights
+            bookings = [booking.to_json for booking in user_bookings]
+            return {f"All bookings for user with uuid {user_id}": bookings}, 200
+
+        return {"Message": f"User with uuid {user_id} doesn't exist in the DB!"}, 404
+
+    except Exception as e:
+        return {"Message": "Couldn't retrieve user's bookings from DB!", "Error": str(e)}, 500
+
+    finally:
+        db.session.close()
+
+@crud_bookings_bp.delete("/users/<uuid:booking_id>")
+def delete_user(booking_id):
+    # TODO: Add BEARER TOKEN
+
+    try:
+        booking = db.session.query(UserBookings).get(booking_id)
+        if booking:
+            db.session.delete(booking)
+            db.session.commit()
+            return {"Message": f"User with uuid {booking_id} was removed successfully from the DB"}, 200
+
+        return {"Message": f"Booking with uuid {booking_id} doesn't exist in the DB!"}, 404
+
+    except Exception as e:
+        db.session.rollback()
+        return {"Message": f"Couldn't delete booking with uuid {booking_id} from DB!", "Error": str(e)}, 500
+
+    finally:
+        db.session.close()
