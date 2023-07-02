56f13895cddce836423aa9193359a53ef8b36162
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2023-07-02 10:09:55 +0300

    Create JWT for users and admins

--- api/routes/login_route.py
+++ api/routes/login_route.py
@@ -1,12 +1,18 @@
+import datetime
+import os
+
+import jwt
+from dotenv import load_dotenv
 from flask import Blueprint, request
 from flask_bcrypt import Bcrypt
 from flask_expects_json import expects_json
-
 from database import db
 from models.users_model import Users
 
 login_bp = Blueprint("login", __name__)
 flask_bcrypt = Bcrypt()
+load_dotenv()
+
 login_schema = {
     'type': 'object',
     'properties': {
@@ -25,14 +31,31 @@ def login_users():
     email = json_data["email"]
     raw_password = json_data["password"]
 
-    check_user = db.session.query(Users).filter_by(email=email).first()
-    if check_user:
-        hashed_user_password = check_user.password
+    user = db.session.query(Users).filter_by(email=email).first()
+    if user:
+        hashed_user_password = user.password
 
         if flask_bcrypt.check_password_hash(hashed_user_password,
                                             raw_password):
-            print()
+            if os.getenv("ADMIN_EMAIL") == user.email and os.getenv(
+                    "ADMIN_PASSWORD") == user.password:
+                payload = {"sub": user.id,
+                           "name": f"{user.first_name} {user.last_name}",
+                           "email": user.email, "admin": True,
+                           "exp": datetime.datetime.utcnow() + datetime.timedelta(
+                               hours=1)}
+            else:
+                payload = {"sub": user.id,
+                           "name": f"{user.first_name} {user.last_name}",
+                           "email": user.email, "admin": False,
+                           "exp": datetime.datetime.utcnow() + datetime.timedelta(
+                               hours=1)}
+
+            token = jwt.encode(payload, os.getenv("SECRET_KEY"),
+                               algorithm="HS256")
+
+            return {"Token": token}
 
         return {"Message": "Invalid password!"}, 401
 
-    return {"Message": "User doesn't exist in the DB"}, 404q
+    return {"Message": "User doesn't exist in the DB"}, 404
