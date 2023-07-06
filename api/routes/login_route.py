62468c83c0762eacc5380485bac5c642291b0427
DanielIvanov19 <49564650+DanielIvanov19@users.noreply.github.com>
2023-07-06 21:14:37 +0300

    36 login register forms (#39)
    Fix #36
    Reformatted code and replaced json approach in register_route with html
    one.
    
    ---------
    
    Co-authored-by: Ivan Obeshkov <ivanobreshkov12@gmail.com>

--- api/routes/login_route.py
+++ api/routes/login_route.py
@@ -4,8 +4,7 @@ import os
 import flask_bcrypt
 import jwt
 from dotenv import load_dotenv
-from flask import Blueprint, request
-from flask_expects_json import expects_json
+from flask import Blueprint, request, render_template
 
 from database import db
 from models.users_model import Users
@@ -25,11 +24,9 @@ login_schema = {
 
 
 @login_bp.post("/login")
-@expects_json(login_schema, check_formats=True)
 def login_users():
-    json_data = request.json
-    email = json_data["email"]
-    raw_password = json_data["password"]
+    email = request.form["email"]
+    raw_password = request.form["password"]
 
     user = db.session.query(Users).filter_by(email=email).first()
     if user:
@@ -38,12 +35,13 @@ def login_users():
         if flask_bcrypt.check_password_hash(hashed_user_password,
                                             raw_password):
             if os.getenv("ADMIN_EMAIL") == user.email and os.getenv(
-                    "ADMIN_PASSWORD") == user.password:
+                    "ADMIN_PASSWORD") == raw_password:
                 payload = {"sub": user.id,
                            "name": f"{user.first_name} {user.last_name}",
                            "email": user.email, "admin": True,
                            "exp": datetime.datetime.utcnow() + datetime.timedelta(
                                hours=1)}
+
             else:
                 payload = {"sub": user.id,
                            "name": f"{user.first_name} {user.last_name}",
@@ -54,8 +52,13 @@ def login_users():
             token = jwt.encode(payload, os.getenv("SECRET_KEY"),
                                algorithm="HS256")
 
-            return {"Token": token}
+            return render_template('login.html', msg=str(token))
 
-        return {"Message": "Invalid password!"}, 401
+        return render_template('login.html', msg="Invalid password!"), 401
 
-    return {"Message": "User doesn't exist in the DB"}, 404
+    return render_template('login.html', msg="User doesn't exist in the DB"), 404
+
+
+@login_bp.get("/login")
+def get_login_form():
+    return render_template("login.html"), 200
