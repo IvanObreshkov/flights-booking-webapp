24ccae66848205ce29dbbd61876c269ce5d64bfd
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-07-27 21:33:51 +0300

    Restructure login endpoint

--- api/routes/login_route.py
+++ api/routes/login_route.py
@@ -29,24 +29,37 @@ def login_users():
         raw_password = data["password"]
 
         user = get_user_by_email(email)
-        if user:
-            hashed_user_password = user.password
-
-            if check_password_hash(hashed_user_password, raw_password):
-
-                token = create_jwt(user, raw_password)
-                resp = make_response(render_template('login.html', msg=str(token)))
-                resp.set_cookie("token", token, httponly=True, secure=True, samesite="Strict")
-                return resp
-
-            return render_template('login.html', msg="Invalid password!"), 401
-
-        return render_template('login.html', msg="User doesn't exist in the DB"), 404
+        return validate_user(raw_password, user), 200
 
     except ValueError as e:
         return render_template('login.html', msg=str(e)), 400
     except Exception as e:
         raise InternalServerError(f'Login failed! Please try again later!, Error: {str(e)}')
+
+
+def validate_user(raw_password, user):
+    """Checks if user exists and check if they entered a valid password
+    Parameters:
+        raw_password: password from login form
+        user: retrieved user obj from the database
+    Returns:
+        200 status code, jwt token and login form if user is valid
+        401 status code if password is Invalid,
+        404 status code if user doesn't exist
+    """
+
+    if user:
+        hashed_user_password = user.password
+
+        if check_password_hash(hashed_user_password, raw_password):
+            token = create_jwt(user, raw_password)
+            resp = make_response(render_template('login.html', msg=str(token)))
+            resp.set_cookie("token", token, httponly=True, secure=True, samesite="Strict")
+            return resp
+
+        return render_template('login.html', msg="Invalid password!"), 401
+    return render_template('login.html', msg="User doesn't exist in the DB"), 404
+
 
 @login_bp.get("/login")
 def get_login_form():
