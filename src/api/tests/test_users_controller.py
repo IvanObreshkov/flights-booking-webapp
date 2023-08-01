3b6bae994653779871805b42f97e5d8191bde8a0
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-01 16:10:30 +0300

    Create tests for validate_data func

--- src/api/tests/test_users_controller.py
+++ src/api/tests/test_users_controller.py
@@ -14,17 +14,6 @@ def test_validate_data_valid():
     assert validate_data(data) is None
 
 
-def test_validate_data_missing_fields():
-    # Missing fields should raise ValueError
-    data = {
-        "first_name": "John",
-        "last_name": "Doe",
-        # Missing "email" and "password"
-    }
-    with pytest.raises(ValueError, match=".* cannot be empty!"):
-        validate_data(data)
-
-
 def test_validate_data_empty_fields():
     # Empty fields should raise ValueError
     data = {
