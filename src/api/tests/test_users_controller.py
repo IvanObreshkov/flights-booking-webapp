62857ea55576ba73f887c96b378f65d61f913167
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-01 20:19:38 +0300

    Add test for get_all_users
    Add test for get_user_by_uuid

--- src/api/tests/test_users_controller.py
+++ src/api/tests/test_users_controller.py
@@ -1,6 +1,9 @@
 import pytest
 
-from api.controllers.users_controller import validate_data
+from api.controllers.users_controller import validate_data, get_all_users, get_user_by_uuid
+from api.models.users_model import Users
+from api.models.flights_model import Flights
+from api.models.user_bookings_model import UserBookings
 
 
 def test_validate_data_valid():
@@ -36,3 +39,39 @@ def test_validate_data_invalid_email():
     }
     with pytest.raises(ValueError, match="Email is not in a valid format!"):
         validate_data(data)
+
+
+def create_mock_user(id, first_name, last_name, email, password):
+    return Users(id=id, first_name=first_name, last_name=last_name, email=email, password=password)
+
+
+def test_get_all_users(mocker):
+    mock_users = [create_mock_user("1", "Ivan", "Obreshkov", "ivan@test.com", "test1234"),
+                  create_mock_user("2", "Toni", "Montana", "toni@toni.bg", "birabira")]
+
+    mock_query = mocker.patch('api.database.db.session.query')
+    mock_query.return_value.all.return_value = mock_users
+
+    result = get_all_users()
+    assert len(result) == 2
+    assert result[0].id == '1'
+    assert result[0].first_name == 'Ivan'
+    assert result[0].last_name == 'Obreshkov'
+    assert result[0].email == 'ivan@test.com'
+
+    assert result[1].id == '2'
+    assert result[1].first_name == 'Toni'
+    assert result[1].last_name == 'Montana'
+    assert result[1].email == 'toni@toni.bg'
+
+def test_get_user_by_uuid(mocker):
+    mock_user = create_mock_user("1", "Ivan", "Obreshkov", "ivan@test.com", "test1234")
+    mock_query = mocker.patch("api.database.db.session.query")
+
+    mock_query.return_value.get.return_value = mock_user
+    result = get_user_by_uuid("1")
+    assert result == mock_user
+
+    mock_query.return_value.get.return_value = None
+    result = get_user_by_uuid("invalid_uuid")
+    assert result is None
