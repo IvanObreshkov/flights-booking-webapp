fc83996513adee79bdeda3a61cd07931afb46f54
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-11 00:30:38 +0300

    Finish test for get_flight_passengers service

--- src/api/tests/test_flights_services.py
+++ src/api/tests/test_flights_services.py
@@ -1,8 +1,12 @@
+from collections import namedtuple
+from unittest.mock import patch, MagicMock
+
 import pytest
-from unittest.mock import patch
-from api.services.flights_services import *
+from sqlalchemy import Row
+
 from api.app import create_app
 from api.config import TestConfig
+from api.services.flights_services import *
 
 
 @pytest.fixture
@@ -101,5 +105,62 @@ def test_get_flight_service_exception(mock_query_flight, app):
     assert status_code == 500
     assert response == {
         "Message": "Couldn't retrieve flight with number F123 from DB!",
+        "Error": "Test exception",
+    }
+
+
+@patch('api.services.flights_services.query_flight_by_flight_number')
+@patch('api.services.flights_services.query_passengers_on_flight')
+def test_get_flight_passengers_service_full_flight(mock_query_passengers, mock_query_flight, app):
+    flight_number = "F123"
+    mock_query_flight.return_value = sample_flight
+    PassengerRow = namedtuple('PassengerRow', ['booking_id', 'uuid', 'email', 'first_name', 'last_name'])
+
+    passengers_data = [
+        PassengerRow(booking_id='70e4c838-a57d-46fc-9050-c94b6ab946e9', uuid='cd3348a0-5164-46c0-8ae1-30787c6cb6ea',
+                     email='dani@gmail.com', first_name='Dani', last_name='Ivanov'),
+        PassengerRow(booking_id='8c8565cf-4384-4474-a8c7-10a62b27ceed', uuid='46916341-3133-4fe0-8520-cc630a185c9f',
+                     email='ivan@gmail.com', first_name='Ivan', last_name='Obreshkov')
+    ]
+    mock_query_passengers.return_value = passengers_data
+
+    response, status_code = get_flight_passengers_service(flight_number)
+    assert status_code == 200
+    assert f"Passengers for flight {flight_number}" in response
+
+
+@patch('api.services.flights_services.query_flight_by_flight_number')
+def test_get_flight_passengers_service_wrong_flight_number(mock_query_flight, app):
+    flight_number = "Wrong number"
+    mock_query_flight.return_value = None
+
+    response, status_code = get_flight_passengers_service(flight_number)
+    assert status_code == 404
+    assert response == {"Message": f"Flight with number {flight_number} doesn't exist in the DB!"}
+
+
+@patch('api.services.flights_services.query_flight_by_flight_number')
+@patch('api.services.flights_services.query_passengers_on_flight')
+def test_get_flight_passengers_service_empty_flight(mock_query_passengers, mock_query_flight, app):
+    flight_number = "F123"
+    mock_query_flight.return_value = sample_flight
+    mock_query_passengers.return_value = []
+
+    response, status_code = get_flight_passengers_service(flight_number)
+    assert status_code == 404
+    assert response == {"Message": f"Flight with number {flight_number} is empty!"}
+
+
+@patch('api.services.flights_services.query_flight_by_flight_number')
+@patch('api.services.flights_services.query_passengers_on_flight')
+def test_get_flight_passengers_service_exception(mock_query_passengers, mock_query_flight, app):
+    flight_number = "F123"
+    mock_query_flight.side_effect = Exception("Test exception")
+    mock_query_passengers.side_effect = Exception("Test exception")
+
+    response, status_code = get_flight_passengers_service(flight_number)
+    assert status_code == 500
+    assert response == {
+        "Message": f"Couldn't retrieve passengers for flight {flight_number} from DB!",
         "Error": "Test exception",
     }
