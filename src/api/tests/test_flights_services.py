ebca346d5f51a222f6baf25fd2d68450a74c731d
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-10 14:44:29 +0300

    Write unit tests for the get_flights_service

--- src/api/tests/test_flights_services.py
+++ src/api/tests/test_flights_services.py
@@ -1,5 +1,5 @@
 import pytest
-
+from unittest.mock import patch
 from api.services.flights_services import *
 from api.app import create_app
 from api.config import TestConfig
@@ -40,9 +40,8 @@ def sample_flight_list():
 
 
 @pytest.fixture()
-def mock_query_all_flights(mocker):
-    return mocker.patch("api.db.repositories.flights_repository.query_all_flights")
-
+def empty_flights_table():
+    return []
 
 @pytest.fixture
 def app():
@@ -51,8 +50,28 @@ def app():
         yield app
 
 
+@patch('api.services.flights_services.query_all_flights')
 def test_get_flights_service_not_empty(mock_query_all_flights, sample_flight_list, app):
     mock_query_all_flights.return_value = sample_flight_list
     response, status_code = get_flights_service()
     assert status_code == 200
     assert "Flights" in response
+
+
+@patch('api.services.flights_services.query_all_flights')
+def test_get_flights_service_empty(mock_query_all_flights, app):
+    mock_query_all_flights.return_value = []
+    response, status_code = get_flights_service()
+    assert status_code == 404
+    assert response == {"Message": "The flights table is empty"}
+
+
+@patch('api.services.flights_services.query_all_flights')
+def test_get_flights_service_exception(mock_query_all_flights, app):
+    mock_query_all_flights.side_effect = Exception("Test exception")
+    response, status_code = get_flights_service()
+    assert status_code == 500
+    assert response == {
+        "Message": "Couldn't retrieve flights from DB!",
+        "Error": "Test exception",
+    }
