94262c51974c52b61b69cf796f36587bf4a7883b
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-25 01:47:11 +0300

    Mock close db session

--- src/api/tests/test_flights_services.py
+++ src/api/tests/test_flights_services.py
@@ -57,7 +57,9 @@ def app():
 @patch('api.services.flights_services.check_flight_existence')
 @patch('api.services.flights_services.add_flight_to_db')
 @patch('api.services.flights_services.create_flight')
-def test_add_flight_service(mock_create_flight, mock_add_flight_to_db, mock_check_flight_existence, app):
+@patch('api.services.flights_services.close_db_session')
+def test_add_flight_service(mock_close_db_session, mock_create_flight, mock_add_flight_to_db,
+                            mock_check_flight_existence):
     mock_request = MagicMock()
     mock_request.json = {"data": "flight_data"}
     mock_check_flight_existence.return_value = False
@@ -66,11 +68,13 @@ def test_add_flight_service(mock_create_flight, mock_add_flight_to_db, mock_chec
     assert status_code == 200
     assert response == {"Message": "New flight added to DB!"}
     mock_add_flight_to_db.called_once_with(sample_flight)
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.check_flight_existence')
 @patch('api.services.flights_services.add_flight_to_db')
-def test_add_flight_service_existing_flight(mock_add_flight_to_db, mock_check_flight_existence, app):
+@patch('api.services.flights_services.close_db_session')
+def test_add_flight_service_existing_flight(mock_close_db_session, mock_add_flight_to_db, mock_check_flight_existence):
     mock_request = MagicMock()
     mock_request.json = {"data": "existing_flight"}
     mock_check_flight_existence.return_value = True
@@ -79,12 +83,15 @@ def test_add_flight_service_existing_flight(mock_add_flight_to_db, mock_check_fl
     assert response == {
         'Message': 'Cannot add the certain flight! A flight with the same data already exist in the database!'}
     mock_add_flight_to_db.assert_not_called()
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.check_flight_existence')
 @patch('api.services.flights_services.add_flight_to_db')
 @patch('api.services.flights_services.create_flight')
-def test_add_flight_service_exception(mock_create_flight, mock_add_flight_to_db, mock_check_flight_existence, app):
+@patch('api.services.flights_services.close_db_session')
+def test_add_flight_service_exception(mock_close_db_session, mock_create_flight, mock_add_flight_to_db,
+                                      mock_check_flight_existence):
     mock_request = MagicMock()
     mock_request.json = {"data": "existing_flight"}
     mock_check_flight_existence.return_value = False
@@ -94,26 +101,32 @@ def test_add_flight_service_exception(mock_create_flight, mock_add_flight_to_db,
     assert status_code == 500
     assert response == {"Message": f"Couldn't create a new flight. Please try again later!",
                         "Error": "Test exception"}
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_all_flights')
-def test_get_flights_service_not_empty(mock_query_all_flights, sample_flight_list, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flights_service_not_empty(mock_close_db_session, mock_query_all_flights, sample_flight_list):
     mock_query_all_flights.return_value = sample_flight_list
     response, status_code = get_flights_service()
     assert status_code == 200
     assert "Flights" in response
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_all_flights')
-def test_get_flights_service_empty(mock_query_all_flights, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flights_service_empty(mock_close_db_session, mock_query_all_flights):
     mock_query_all_flights.return_value = []
     response, status_code = get_flights_service()
     assert status_code == 404
     assert response == {"Message": "The flights table is empty"}
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_all_flights')
-def test_get_flights_service_exception(mock_query_all_flights, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flights_service_exception(mock_close_db_session, mock_query_all_flights):
     mock_query_all_flights.side_effect = Exception("Test exception")
     response, status_code = get_flights_service()
     assert status_code == 500
@@ -121,26 +134,32 @@ def test_get_flights_service_exception(mock_query_all_flights, app):
         "Message": "Couldn't retrieve flights from DB!",
         "Error": "Test exception",
     }
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
-def test_get_flight_service_existing(mock_query_flight, sample_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_service_existing(mock_close_db_session, mock_query_flight, sample_flight):
     mock_query_flight.return_value = sample_flight
     response, status_code = get_flight_service("F123")
     assert status_code == 200
     assert "Flight" in response
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
-def test_get_flight_service_non_existing(mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_service_non_existing(mock_close_db_session, mock_query_flight):
     mock_query_flight.return_value = None
     response, status_code = get_flight_service("Wrong FN")
     assert status_code == 404
     assert response == {"Message": "Flight with number Wrong FN doesn't exist in the DB!"}
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
-def test_get_flight_service_exception(mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_service_exception(mock_close_db_session, mock_query_flight):
     mock_query_flight.side_effect = Exception("Test exception")
     response, status_code = get_flight_service("F123")
     assert status_code == 500
@@ -148,11 +167,13 @@ def test_get_flight_service_exception(mock_query_flight, app):
         "Message": "Couldn't retrieve flight with number F123 from DB!",
         "Error": "Test exception",
     }
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.query_passengers_on_flight')
-def test_get_flight_passengers_service_full_flight(mock_query_passengers, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_passengers_service_full_flight(mock_close_db_session, mock_query_passengers, mock_query_flight):
     flight_number = "F123"
     mock_query_flight.return_value = sample_flight
     PassengerRow = namedtuple('PassengerRow', ['booking_id', 'uuid', 'email', 'first_name', 'last_name'])
@@ -168,21 +189,25 @@ def test_get_flight_passengers_service_full_flight(mock_query_passengers, mock_q
     response, status_code = get_flight_passengers_service(flight_number)
     assert status_code == 200
     assert f"Passengers for flight {flight_number}" in response
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
-def test_get_flight_passengers_service_wrong_flight_number(mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_passengers_service_wrong_flight_number(mock_close_db_session, mock_query_flight):
     flight_number = "Wrong number"
     mock_query_flight.return_value = None
 
     response, status_code = get_flight_passengers_service(flight_number)
     assert status_code == 404
     assert response == {"Message": f"Flight with number {flight_number} doesn't exist in the DB!"}
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.query_passengers_on_flight')
-def test_get_flight_passengers_service_empty_flight(mock_query_passengers, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_passengers_service_empty_flight(mock_close_db_session, mock_query_passengers, mock_query_flight):
     flight_number = "F123"
     mock_query_flight.return_value = sample_flight
     mock_query_passengers.return_value = []
@@ -190,11 +215,13 @@ def test_get_flight_passengers_service_empty_flight(mock_query_passengers, mock_
     response, status_code = get_flight_passengers_service(flight_number)
     assert status_code == 404
     assert response == {"Message": f"Flight with number {flight_number} is empty!"}
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.query_passengers_on_flight')
-def test_get_flight_passengers_service_exception(mock_query_passengers, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_get_flight_passengers_service_exception(mock_close_db_session, mock_query_passengers, mock_query_flight):
     flight_number = "F123"
     mock_query_flight.side_effect = Exception("Test exception")
     mock_query_passengers.side_effect = Exception("Test exception")
@@ -205,11 +232,13 @@ def test_get_flight_passengers_service_exception(mock_query_passengers, mock_que
         "Message": f"Couldn't retrieve passengers for flight {flight_number} from DB!",
         "Error": "Test exception",
     }
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.delete_flight_from_db')
-def test_delete_flight_service_existing_flight(mock_delete_flight, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_delete_flight_service_existing_flight(mock_close_db_session, mock_delete_flight, mock_query_flight):
     flight_number = "F123"
     mock_query_flight.return_value = sample_flight
 
@@ -217,11 +246,13 @@ def test_delete_flight_service_existing_flight(mock_delete_flight, mock_query_fl
     assert status_code == 200
     assert response == {"Message": f"Flight with number: {flight_number} was removed successfully from the DB"}
     mock_delete_flight.assert_called_once()
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.delete_flight_from_db')
-def test_delete_flight_service_wrong_flight_number(mock_delete_flight, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_delete_flight_service_wrong_flight_number(mock_close_db_session, mock_delete_flight, mock_query_flight):
     flight_number = "Wrong number"
     mock_query_flight.return_value = None
 
@@ -229,11 +260,15 @@ def test_delete_flight_service_wrong_flight_number(mock_delete_flight, mock_quer
     assert status_code == 404
     assert response == {"Message": f"Flight with number: {flight_number} doesn't exist in the DB!"}
     mock_delete_flight.assert_not_called()
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.delete_flight_from_db')
-def test_delete_flight_service_exception(mock_delete_flight, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+@patch("api.services.flights_services.db_rollback")
+def test_delete_flight_service_exception(mock_db_rollback, mock_close_db_session, mock_delete_flight,
+                                         mock_query_flight):
     flight_number = "F123"
     mock_delete_flight.side_effect = Exception("Test exception")
     mock_query_flight.side_effect = Exception("Test exception")
@@ -243,11 +278,14 @@ def test_delete_flight_service_exception(mock_delete_flight, mock_query_flight, 
     assert response == {"Message": f"Couldn't delete flight with number: {flight_number} from DB!",
                         "Error": "Test exception"}
     mock_delete_flight.assert_not_called()
+    mock_close_db_session.assert_called_once()
+    mock_db_rollback.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.edit_flight_data')
-def test_update_flight_service_existing_flight(mock_edit_flight_data, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_update_flight_service_existing_flight(mock_close_db_session, mock_edit_flight_data, mock_query_flight):
     flight_number = "F123"
     mock_query_flight.return_value = sample_flight
     request = MagicMock()
@@ -256,11 +294,13 @@ def test_update_flight_service_existing_flight(mock_edit_flight_data, mock_query
     assert status_code == 200
     assert response == {"Message": f"Flight with number: {flight_number} was updated successfully."}
     mock_edit_flight_data.assert_called_once_with(sample_flight, request.json)
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.edit_flight_data')
-def test_update_flight_service_wrong_flight_number(mock_edit_flight_data, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+def test_update_flight_service_wrong_flight_number(mock_close_db_session, mock_edit_flight_data, mock_query_flight):
     flight_number = "Wrong number"
     mock_query_flight.return_value = None
     request = MagicMock()
@@ -269,11 +309,15 @@ def test_update_flight_service_wrong_flight_number(mock_edit_flight_data, mock_q
     assert status_code == 404
     assert response == {"Message": f"Flight with number: {flight_number} doesn't exist in the DB!"}
     mock_edit_flight_data.assert_not_called()
+    mock_close_db_session.assert_called_once()
 
 
 @patch('api.services.flights_services.query_flight_by_flight_number')
 @patch('api.services.flights_services.edit_flight_data')
-def test_update_flight_service_exception(mock_edit_flight_data, mock_query_flight, app):
+@patch('api.services.flights_services.close_db_session')
+@patch("api.services.flights_services.db_rollback")
+def test_update_flight_service_exception(mock_db_rollback, mock_close_db_session, mock_edit_flight_data,
+                                         mock_query_flight):
     flight_number = "F123"
     mock_query_flight.side_effect = Exception("Test exception")
     mock_edit_flight_data.side_effect = Exception("Test exception")
@@ -284,3 +328,5 @@ def test_update_flight_service_exception(mock_edit_flight_data, mock_query_fligh
     assert response == {'Message': "Couldn't update flight with number: F123",
                         "Error": "Test exception"}
     mock_edit_flight_data.assert_not_called()
+    mock_close_db_session.assert_called_once()
+    mock_db_rollback.assert_called_once()
