3734dccbdf8598f5a9a29cd5b539c738f8f969f2
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-27 17:31:40 +0300

    Create a validate endpoint for verification email

--- src/api/routes/verification_routes.py
+++ src/api/routes/verification_routes.py
@@ -5,6 +5,6 @@ from api.services.verification_service import Verification
 verification_bp = Blueprint("verification", __name__)
 
 @verification_bp.get("/verify")
-def get_verification_token():
+def verify_user_route():
     verification_token = request.args["token"]
     return Verification.verify_user(verification_token)
